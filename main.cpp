6200bc954e9b53f7d2aeed6c8a423c73c20d39c7
GabrieleSokaite <71820954+GabrieleSokaite@users.noreply.github.com>
2020-09-25 03:04:06 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,61 +1,76 @@
 #include <iostream>
-#include <string>
-#include <vector>
-#include <ctime>
 #include <cstdlib>
-#include <bits/stdc++.h>
-#include <sstream>
+#include <ctime>
 
 using namespace std;
 using std:: cout;
 using std:: cin;
-using std:: string;
-using std:: vector;
 using std:: endl;
-
 
 struct duomuo {
     string vard, pav;
-    int paz[10], egz;
+    int paz, egz, gen, skc;
     float gp = 0;
 };
+
+bool ar(string s)
+{
+    for (int i = 0; i < s.length(); i++)
+        if (isdigit(s[i]) == false)
+            return false;
+
+    return true;
+}
 
 int main()
 {
     duomuo Eil; duomuo Eil_mas[5];
 
-    cout << "Iveskite varda, pavarde ir 5 namu darbu pazymius duomenis: \n";
+    cout << "Ar norite generuoti skaicius? (Pasirinkimai: 1 atitinka taip, 2 atitinka ne): \n";
+    cin >> Eil.gen;
+    if (Eil.gen == 1) /*generuoja skaicius*/
+    {
+        cout << "Iveskite kiek skaiciu generuoti norite: \n";
+        srand((unsigned) time(0));
+        int random, n;
+        cin >> n;
+        for (int index = 0; index < n; index++)
+            {
+                random = (rand() % 10) + 1;
+                cout << random << endl;
+            }
+    }
+    else if (Eil.gen == 2) /*skaiciai ivedami ranka*/
+    {
+        cout << "Iveskite egzamino pazymi. Iveskite namu darbu pazymius (kai ivedete visus parasykite 000): \n";
+        cin >> Eil.egz;
+        if (Eil.paz != 000) cin >> Eil.paz;
+    }
+    else
+        cout << "Negalimas toks ivedimas.\n" <<endl;
+    cout << "Iveskite ka norite skaiciuoti. Variantai: 1 atitinka vidurki, 2 atitinka mediana): \n";
+    cin >> Eil.skc;
+    if (Eil.skc == 1) /*skaiciuosim vidurki*/
+    {
+
+    }
+    else if (Eil.skc == 2) /*skaiciuosim mediana*/
+    {
+
+    }
+    else
+        cout << "Nepasirinkote ka norite skaiciuoti.\n";
+    /*vector konteineris*/
+    cout << "Iveskite varda, pavarde: \n";
     cin >> Eil.vard >> Eil.pav;
-
-    for (int i=0; i<5; i++) {
-        cin >> Eil.paz[i];
-        Eil.gp = Eil.gp + (float) Eil.paz[i];
-    }
-
-    int max;
-    cout << "Iveskite max galima egzamino bala: \n";
-    cin >> max;
-    if (max > 0 && max <= 10)
-    {
-        srand(time(0));
-        Eil.egz = (rand () % max) + 1;
-    cout << Eil.egz << endl;
-    }
+    if (ar(Eil.vard))
+        cout << "Netinkamas vardas\n";
     else
-        cout << "Nera tokio pazymio \n";
-        system ("pause");
-
-    if (Eil.egz > 0 && Eil.egz <= 10)
-    {
-        Eil.gp = Eil.gp / 5.0;
-        Eil.gp = Eil.gp * 0.4 + 0.6 * Eil.egz;
-    }
+        cout << Eil.vard << "   " << Eil.pav << endl;
+    if (ar(Eil.pav))
+        cout << "Netinkama pavarde \n";
     else
-        Eil.gp = Eil.gp / 5.0 * 0.4;
-
-    cout << "Vardas" << "   " << "Pavarde" << "     " << "Galutinis pazymys" << endl;
-    cout << Eil.vard << "   " << Eil.pav;
-    cout << " " << Eil.gp << endl;
+        cout << Eil.vard << "   " << Eil.pav;
 
     char a;
     cin >> a;
